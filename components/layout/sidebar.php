c253786a79e7a5c2e2ba246da682511ee9c493fa
Khasbi <khasbi@MacBook-Air-Khasbi.local>
2024-10-29 08:08:15 +0700

    upload project

---
+++ components/layout/sidebar.php
@@ -0,0 +1,43 @@
+<div class="main-sidebar sidebar-style-2">
+    <aside id="sidebar-wrapper">
+        <div class="sidebar-brand">
+            <a href="index.html">POS PADANG</a>
+        </div>
+        <div class="sidebar-brand sidebar-brand-sm">
+            <a href="index.html">St</a>
+        </div>
+        <ul class="sidebar-menu">
+            <li><a class="nav-link" href="<?php __DIR__ . '/' ?>"><i class="fas fa-fire"></i> <span>Dashboard</span></a></li>
+            <li class="menu-header">Product</li>
+            <li class="dropdown">
+                <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="far fa-calendar-minus"></i></i> <span>Categori</span></a>
+                <ul class="dropdown-menu">
+                    <li><a class="nav-link" href="/categori">Home Categori</a></li>
+                    <li><a class="nav-link" href="layout-default.html">Tambah Categori</a></li>
+                </ul>
+            </li>
+            <li class="dropdown">
+                <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-utensils"></i> <span>Menu</span></a>
+                <ul class="dropdown-menu">
+                    <li><a class="nav-link" href="layout-default.html">Home Menu</a></li>
+                    <li><a class="nav-link" href="layout-default.html">Tambah Menu</a></li>
+                </ul>
+            </li>
+            <li class="menu-header">Transaksi</li>
+            <li class="dropdown">
+                <a href="#" class="nav-link has-dropdown"><i class="fas fa-money-bill-wave"></i> <span>Pemesanan</span></a>
+                <ul class="dropdown-menu">
+                    <li><a class="nav-link" href="components-article.html">Home Pesanan</a></li>
+                    <li><a class="nav-link" href="components-article.html">Buat Pesanan</a></li>
+                </ul>
+            </li>
+            <li class="menu-header">Akun</li>
+            <li class="dropdown">
+                <a href="#" class="nav-link has-dropdown"><i class="fas fa-user-alt"></i> <span>Akun</span></a>
+                <ul class="dropdown-menu">
+                    <li><a class="nav-link" href="components-article.html">Informasi Akun</a></li>
+                </ul>
+            </li>
+        </ul>
+    </aside>
+</div>
---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+
+// require_once __DIR__ . '/Model/Car.php';
+// require_once __DIR__ . '/Model/Owner.php';
+
+// // $model = new Model();
+// // $car = new Car();
+// $data_mobil = ['name' => 'Pajelo', 'brand' => 'Mitsubishi Motors', 'color' => 'Merah'];
+// $data_owner = ['name' => 'Honda', 'country' => 'Jepang', 'since' => '1956'];
+
+// // var_dump($car->all());
+// // echo "<br>";
+// // var_dump($car->create($data_mobil));
+// // echo "<br>";
+// // var_dump($car->find(1));
+// // echo "<br>";
+// // var_dump($car->update(2, $data_mobil));
+// // echo "<br>";
+// // var_dump($car->delete(2));
+
+// $honda = new Owner();
+// var_dump($honda->create($data_owner));
+// echo "<br>";
+// echo "<br>";
+// var_dump($honda->all());
+// echo "<br>";
+// echo "<br>";
+// var_dump($honda->find(1));
+// echo "<br>";
+// echo "<br>";
+// var_dump($honda->update(1, $data_owner));
+// echo "<br>";
+// echo "<br>";
+// var_dump($honda->delete(1));
+
+header("Location: /projects/pos/views/index.php");
